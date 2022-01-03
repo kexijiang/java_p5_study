59e89deb12c1334ed0f081ebb608fe763eed2038
姜克喜 <kxjiang01@163.com>
2022-01-03 17:58:34 +0800

    2022-01-03提交

---
+++ src/main/java/com/kxjiang/java_p5_study/JavaP5StudyApplication.java
@@ -0,0 +1,13 @@
+package com.kxjiang.java_p5_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaP5StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaP5StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kxjiang/java_p5_study/JavaP5StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kxjiang.java_p5_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaP5StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
