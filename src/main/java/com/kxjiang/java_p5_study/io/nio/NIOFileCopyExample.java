34da94bfedf74f24a1b9cd56fcc48ec421904f8e
姜克喜 <kxjiang01@163.com>
2022-03-28 23:11:31 +0800

    2022-03-28提交

---
+++ src/main/java/com/kxjiang/java_p5_study/io/nio/NIOFileCopyExample.java
@@ -0,0 +1,39 @@
+package com.kxjiang.java_p5_study.io.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author kxjiang
+ * @date 2022-03-28 22:46
+ */
+public class NIOFileCopyExample {
+    public static void main(String[] args) {
+        try{
+            long start = System.currentTimeMillis();
+            FileInputStream fis = new FileInputStream("E:\\test.txt");
+            FileOutputStream fos = new FileOutputStream("E:\\test-copy.txt");
+            // NIO的api
+            FileChannel fin = fis.getChannel();
+            FileChannel fout = fos.getChannel();
+
+            // 初始化一个缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while(fin.read(buffer)!=-1){
+                // 从读转化为写
+                buffer.flip();
+                // 从缓冲区写入到通道
+                fout.write(buffer);
+                //清空缓冲区
+                buffer.clear();
+            }
+
+            System.out.println("NIO复制文件用时："+(System.currentTimeMillis()-start)+" ms");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/kxjiang/java_p5_study/io/nio/NIOReadExample.java
@@ -0,0 +1,27 @@
+package com.kxjiang.java_p5_study.io.nio;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author kxjiang
+ * @date 2022-03-28 23:03
+ */
+public class NIOReadExample {
+    public static void main(String[] args) {
+        try(FileInputStream inputStream = new FileInputStream("E:\\test.txt")){
+            FileChannel fileChannel = inputStream.getChannel();
+            // 一次读取字节数
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+            // 如果rs = -1 代表文件读取结束
+            int rs = fileChannel.read(byteBuffer);
+
+            System.out.println(new String(byteBuffer.array()));
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
